e080bc5c3b351e85023adbed4c2cbb4f59681229
Brandon Blaylock <brandon@null.pub>
2019-07-02 13:10:01 -0700

    feat: add meta parameter to asyncMapFactory projection function

--- src/rxjs/asyncMap.ts
+++ src/rxjs/asyncMap.ts
@@ -1,19 +1,19 @@
 import { from, Observable, of } from 'rxjs';
 import { catchError, concatMap, exhaustMap, filter, map, mergeMap, switchMap } from 'rxjs/operators';
 
-import { Action, AsyncActionCreators, Meta, ActionCreator } from '../actions/interfaces';
+import { Action, ActionCreator, AsyncActionCreators, Meta } from '../actions/interfaces';
 
 /**
  * Takes a lettable map operator and creates asyncMap operator
  */
 const asyncMapFactory = (mapper: typeof mergeMap) => <P, R, E, M extends Meta>(
   action: AsyncActionCreators<P, R, E, M>,
-  project: (params: P) => Observable<R>
+  project: (params: P, meta: M) => Observable<R>
 ) => (obs: Observable<Action<any, any>>) =>
   obs.pipe(
     filter(action.pending.match),
     mapper(({ payload: params, meta }) =>
-      project(params).pipe(
+      project(params, meta).pipe(
         map(result => action.success({ params, result }, meta)),
         catchError(error => of(action.failure({ params, error }, meta)))
       )
@@ -38,5 +38,7 @@ export const mapAction = <P, M extends Meta>(
 ) => (obs: Observable<Action<any, any>>) =>
   obs.pipe(
     filter(action.match),
-    mergeMap(action => from((keepAction ? [action] : []).concat(project(action))))
+    mergeMap(action =>
+      from((keepAction ? [action] : []).concat(project(action)))
+    )
   );
