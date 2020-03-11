5278ca2be44b1ed56e81bc3488450ccaaf9289a0
Brandon Blaylock <brandon@null.pub>
2020-03-11 16:22:45 -0700

    chore: fix sourcemaps in dist

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,7 +1,12 @@
 ///<reference path="../node_modules/@types/node/index.d.ts" />
 import * as fs from "fs";
+import * as path from "path";
 
-const buildDir = "./dist";
+/**
+ * Treat package.json root as root.
+ */
+const outDir = "./dist";
+const copyItems = ["README.md", "src"]; // For Sourcemaps
 
 const pkg = require("../package.json");
 
@@ -20,10 +25,48 @@ const publishPackage = {
   dependencies: pkg.dependencies
 };
 
-if (!fs.existsSync(buildDir)) {
-  fs.mkdirSync(buildDir);
+const copyRecursiveSync = (src: string, dest: string): void => {
+  const srcIsDirectory = fs.statSync(src).isDirectory();
+  if (srcIsDirectory) {
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+    });
+  } else {
+    fs.copyFileSync(src, dest); // UPDATE FROM:    fs.linkSync(src, dest);
+  }
+};
+
+const remapSources = (map: { sources: string[] }) => {
+  const sources = map.sources.map(source => source.replace(/\.\.\/src/, "./src"));
+  return {
+    ...map,
+    sources
+  };
+};
+
+if (!fs.existsSync(outDir)) {
+  fs.mkdirSync(outDir);
 }
 
-fs.writeFileSync([buildDir, "package.json"].join("/"), JSON.stringify(publishPackage, null, 2), {
+// Write package.json
+fs.writeFileSync([outDir, "package.json"].join("/"), JSON.stringify(publishPackage, null, 2), {
   encoding: "utf-8"
+});
+
+// Copy additional items
+copyItems.forEach(src => copyRecursiveSync(src, [outDir, src].join("/")));
+
+// Modify sourcemaps
+const sourceMaps = fs
+  .readdirSync(outDir)
+  .filter(file => file.endsWith("js.map"))
+  .map(file => [outDir, file].join("/"));
+
+sourceMaps.forEach(map => {
+  const file = JSON.parse(fs.readFileSync(map, { encoding: "utf-8" }));
+  const newFile = remapSources(file);
+  fs.writeFileSync(map, JSON.stringify(newFile), { encoding: "utf-8" });
 });
